56d5bbc680957bbc28e442cab19ac57ac60fca90
Alexander Letnikow <reclamme@gmail.com>
2019-06-18 03:19:11 +0300

    Refactor to use React Hooks

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
-import * as Calculator from "react-credit-calculator";
+import { ControlContext } from "react-credit-calculator";
 import BaseSlider, { SliderProps } from "rc-slider";
 
-export const Slider: React.FunctionComponent<SliderProps> = (props) => (
-    <Calculator.ControlContext.Consumer>
-        {(value: Calculator.ControlContextValue) => <BaseSlider {...props} {...value} />}
-    </Calculator.ControlContext.Consumer>
-);
+export const Slider: React.FC<SliderProps> = (props) => {
+    const context = React.useContext(ControlContext);
+    return <BaseSlider {...props} {...context} />;
+};
